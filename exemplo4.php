e437fea329d9244464fe56aa6b2ef3b629dbc2d9
guilherme-matte <guiguimatte@gmail.com>
2024-08-28 11:40:15 -0300

    att

---
+++ exemplo4.php
@@ -0,0 +1,16 @@
+<?php
+
+$n1 = 110;
+$n2 = 9;
+$total = $n1 + $n2;
+
+echo 'Resultado da adição de n1 e n2: ', $total;
+echo '<br>';
+$total = $n1 - $n2;
+echo 'Resultado da subtração de n1 e n2: ', $total;
+echo '<br>';
+$total = $n1 * $n2;
+echo 'Resultado da multiplicação de n1 e n2: ', $total;
+echo '<br>';
+$total = $n1 / $n2;
+printf('Resultado da divisão de n1 e n2:  %2.2f', $total);
---
+++ exemplo5.php
@@ -0,0 +1,21 @@
+<?php
+
+$n1 = 8;
+$n2 = 2;
+$n3 = 5;
+$n4 = 6;
+$n5 = 9;
+$media = ($n1 + $n2 + $n3 + $n4 + $n5) / 5;
+echo 'Valor da variavel 1: ', $n1;
+
+echo '<br/>';
+echo 'Valor da variavel 2: ', $n2;
+echo '<br/>';
+echo 'Valor da variavel 3: ', $n3;
+echo '<br/>';
+echo 'Valor da variavel 4: ', $n4;
+echo '<br/>';
+echo 'Valor da variavel 5: ', $n5;
+echo '<br/>';
+printf('Media final: %.2f', $media);
+
