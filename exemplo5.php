a25f4a6b2dd666f8f9da174842f6dfad81bc249d
guilherme-matte <guiguimatte@gmail.com>
2024-08-28 11:49:44 -0300

    att

--- exemplo5.php
+++ exemplo5.php
@@ -6,16 +6,16 @@ $n3 = 5;
 $n4 = 6;
 $n5 = 9;
 $media = ($n1 + $n2 + $n3 + $n4 + $n5) / 5;
-echo 'Valor da variavel 1: ', $n1;
+echo 'Valor da nota 1: ', $n1;
 
 echo '<br/>';
-echo 'Valor da variavel 2: ', $n2;
+echo 'Valor da nota 2: ', $n2;
 echo '<br/>';
-echo 'Valor da variavel 3: ', $n3;
+echo 'Valor da nota 3: ', $n3;
 echo '<br/>';
-echo 'Valor da variavel 4: ', $n4;
+echo 'Valor da nota 4: ', $n4;
 echo '<br/>';
-echo 'Valor da variavel 5: ', $n5;
+echo 'Valor da nota 5: ', $n5;
 echo '<br/>';
 printf('Media final: %.2f', $media);
 
