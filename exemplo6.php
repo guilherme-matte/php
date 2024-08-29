50236f037560bf828aaa962c94f62edd61011410
guilherme-matte <guiguimatte@gmail.com>
2024-08-29 10:33:06 -0300

    Update exemplo6.php

--- exemplo6.php
+++ exemplo6.php
@@ -26,3 +26,44 @@ function nomeIdade($nome, $idade) {
 
 nomeIdade("Guilherme", 21);
 nomeIdade("Clovis", 7);
+
+/*
+ *  O escopo de uma variavel é o cibtexti onde ela foi definida.
+ *  A maioria das variáveis do PHP tem somento o escopo local.
+ * No PHP, as variaveis globais precisam se declaradas como globais dentro de uma função
+ * 
+ */
+echo '<gr>';
+
+function soma($a) {
+    global $b;
+    $b = $a + 10;
+}
+
+soma(10);
+echo 'Valor da soma é: ', $b;
+echo '<hr>';
+
+function somaReturn($a) {
+    global $b;
+
+    $b = $a + 10;
+
+    return $b;
+}
+
+echo 'Valor da soma é: ', somaReturn(10);
+echo '<hr>';
+$d = 10;
+$c = 20;
+
+function calcular() {
+    global $c, $d, $e;
+
+    $e = $c + $d;
+
+    return $e;
+}
+
+echo calcular();
+
