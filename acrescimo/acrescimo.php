f6ddfcb777f9fcb187a3b67680091c7320177799
guilherme-matte <guiguimatte@gmail.com>
2024-09-09 11:56:25 -0300

    att

---
+++ acrescimo/acrescimo.php
@@ -0,0 +1,10 @@
+<?php
+
+$valor = $_POST['valor'];
+$diasAtraso = $_POST['diasAtraso'];
+$juros = $_POST['jurosMensal'];
+
+
+
+$total = $valor + ($valor * ($juros / 100) * ($diasAtraso / 30) );
+echo 'Valor total da prestação R$: ' . $total;
