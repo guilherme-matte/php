fa1577f836ce2cf9adcea9b222a0313a37e72f64
guilherme-matte <guiguimatte@gmail.com>
2024-11-25 22:45:42 -0300

    att

---
+++ Products/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/reset.css" type="text/css">
+    <link rel="stylesheet" href="./css/layout.css" type="text/css">
+
+    <title>Produtos</title>
+</head>
+
+<body>
+    <a href="index.php">Menu Principal</a>
+    <a href="./pages/cadProd.php">Cadastrar Produto</a>
+    <a href="./pages/pesqProd.php">Pesquisar Produto</a>
+    <hr>
+    <p class="pTitulo">Todos os Produtos</p>
+
+
+    <hr>
+</body>
+
+</html>
---
+++ Products/pages/cadProd.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/reset.css" type="text/css">
+    <link rel="stylesheet" href="../css/layout.css" type="text/css">
+
+    <title>Cadastrar</title>
+</head>
+
+<body>
+    <a href="../index.php">Menu Principal</a>
+    <a href="cadProd.php">Cadastrar Produto</a>
+    <a href="pesqProd.php">Pesquisar Produto</a>
+    <hr>
+    <p class="pTitulo">Cadastrar produto</p>
+
+    <hr>
+</body>
+
+</html>
