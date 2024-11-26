969222ce9ffd442b3ee9049e4a84fd4b3573fe09
guilherme-matte <guiguimatte@gmail.com>
2024-11-26 17:31:50 -0300

    att

--- Products/index.php
+++ Products/index.php
@@ -17,7 +17,67 @@
     <hr>
     <p class="pTitulo">Todos os Produtos</p>
 
+    <?php
+    $url = "http://localhost:8080/products";
 
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($ch);
+
+    if ($response === false) {
+        $error = curl_error($ch);
+
+        echo 'ERRO na requisição 1';
+    } else {
+        $data = json_decode($response, true);
+        //var_dump($data);
+        if ($data && is_array($data)) {
+            foreach ($data as $product) {
+                echo 'aa';
+                $id = $product['idproduct'];
+                $name = $product['name'];
+                $value = $product['value'];
+
+                echo "ID: $id<br>";
+                echo "Nome: $name<br>";
+                echo "Valor: $value<br>";
+
+                $urlRate = "http://localhost:8080/products/" . $id . "/rate";
+
+                $chRate = curl_init();
+
+                curl_setopt($chRate, CURLOPT_URL, $urlRate);
+
+                curl_setopt($chRate, CURLOPT_RETURNTRANSFER, true);
+
+                $responseRate = curl_exec($chRate);
+
+                if ($responseRate === false) {
+                    $error = curl_error($chRate);
+
+                    echo 'ERRO na requisição 2 ' . $error;
+                } else {
+                    $dataRate = json_decode($responseRate, true);
+                    if ($dataRate && is_array($dataRate)) {
+                        foreach ($dataRate as $rate)
+                            if ($rate['quantity'] == 0 || $rate['allRate'] == null) {
+                                echo 'Avaliação: 0';
+                            } else {
+                                $media = $rate['allRate'] / $rate['quantity'];
+                                echo 'Avaliação' . $media;
+                            }
+                    }
+                }
+            }
+        } else {
+            echo 'Nenhum dado encontrado';
+        }
+    }
+    ?>
     <hr>
 </body>
 
