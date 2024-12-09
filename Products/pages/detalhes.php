2f289c119edb7af97c1640851a94ed25f55f083a
guilherme-matte <guiguimatte@gmail.com>
2024-12-09 16:12:35 -0300

    Update detalhes.php

--- Products/pages/detalhes.php
+++ Products/pages/detalhes.php
@@ -91,6 +91,23 @@ session_start();
         }
     }
     ?>
+    <div id="rating">
+        <span class="star" data-value="1">&#9733;</span>
+        <span class="star" data-value="2">&#9733;</span>
+        <span class="star" data-value="3">&#9733;</span>
+        <span class="star" data-value="4">&#9733;</span>
+        <span class="star" data-value="5">&#9733;</span>
+    </div>
+    <p id="ratingValue"></p>
+    <script>
+        const stars = document.querySelectorAll('.star');
+        const ratingValue =document.getElementById('ratingValue');
+
+        stars.forEachstar(star=>{
+            
+
+        })
+    </script>
 </body>
 
 </html>
